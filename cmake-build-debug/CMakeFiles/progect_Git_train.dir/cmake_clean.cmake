778f43605e00c40205756058cfcc0df5291eb06e
Wład Statkewicz <kirillwiller@MacBook-Pro-Wad-Statkewicz.local>
2021-02-04 18:18:54 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/progect_Git_train.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/progect_Git_train.dir/main.cpp.o"
+  "progect_Git_train"
+  "progect_Git_train.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/progect_Git_train.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/progect_Git_train.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for progect_Git_train.
+# This may be replaced when dependencies are built.
